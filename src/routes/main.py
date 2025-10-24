c4fff176cf7151f3ecdeb4dcdf08423121dc91fd
JoaoBarreto3 <jv.barreto80@gmail.com>
2025-10-24 10:10:27 -0300

    Fix: Correçãp nas mudanças da branch main para back-end

--- src/routes/main.py
+++ src/routes/main.py
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-from flask import Blueprint, render_template
-
-main_bp = Blueprint("main", __name__)
-# AQUI DEFINIMOS TODAS AS ROTAS PRINCIPAIS
-# COMO HOME, SOBRE, EQUIPE, DOCUMENTACAO
-@main_bp.route("/")
-def home():
-    return render_template("index.html")
-
-@main_bp.route("/sobre")
-def sobre():
-    return render_template("pages/sobrenos.html")
-
-
-@main_bp.route("/processos")
-def processos():
-    return render_template("pages/processos.html")
-
-
-@main_bp.route("/metodos")
-def metodos():
-    return render_template("pages/metodos.html")
-=======
 from flask import Blueprint, render_template
 
 main_bp = Blueprint("main", __name__)
@@ -49,12 +25,6 @@ def Avaliação():
     return render_template("pages/Avaliação.html")
 
 
-@main_bp.route("/quiz")
-def quiz():
-    return render_template("pages/quiz.html")
-
-
 @main_bp.route("/metodos")
 def metodos():
     return render_template("pages/metodos.html")
->>>>>>> main
